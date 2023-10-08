d23f8596fa9b63b085aaeb8e9ca11f05c7aa5b21
Sameer Yadav <sameer@Sameers-MacBook-Air.local>
2023-10-08 22:23:48 +0530

    changed endpoints

--- src/utils/apiRoutes.js
+++ src/utils/apiRoutes.js
@@ -33,35 +33,35 @@ export function apiRoutes() {
   /////////////////////////////////////////////////////////////////////////////////////////////////
 
   // // server
-  const loginAPI = "http://65.2.35.62:9002/api/login";
-  const registerAPI = "http://65.2.35.62:9002/api/register";
-  const getJobsListAPI = `http://65.2.35.62:9002/api`;
-  const updateJobAPI = `http://65.2.35.62:9002/api`;
-  const addJobAPI = "http://65.2.35.62:9002/api/jobs/addJob";
-  const mainReportAPI = `http://65.2.35.62:9002/api/report`;
-  const importerListAPI = "http://65.2.35.62:9002/api/getImporterList";
-  const jobsOverviewAPI = "http://65.2.35.62:9002/api/getJobsOverview";
-  const importerJobsAPI = "http://65.2.35.62:9002/api/getImporterJobs";
-  const assignJobsAPI = "http://65.2.35.62:9002/api/assignJobs";
-  const getUsersAPI = "http://65.2.35.62:9002/api/getUsers";
-  const getUsersWithJobsAPI = "http://65.2.35.62:9002/api/getUsersWithJobs";
-  const updateJobsDateAPI = "http://65.2.35.62:9002/api/updateJobsDate";
-  const getLastJobsDateAPI = "http://65.2.35.62:9002/api/getLastJobsDate";
+  const loginAPI = "http://3.108.185.65:9002/api/login";
+  const registerAPI = "http://3.108.185.65:9002/api/register";
+  const getJobsListAPI = `http://3.108.185.65:9002/api`;
+  const updateJobAPI = `http://3.108.185.65:9002/api`;
+  const addJobAPI = "http://3.108.185.65:9002/api/jobs/addJob";
+  const mainReportAPI = `http://3.108.185.65:9002/api/report`;
+  const importerListAPI = "http://3.108.185.65:9002/api/getImporterList";
+  const jobsOverviewAPI = "http://3.108.185.65:9002/api/getJobsOverview";
+  const importerJobsAPI = "http://3.108.185.65:9002/api/getImporterJobs";
+  const assignJobsAPI = "http://3.108.185.65:9002/api/assignJobs";
+  const getUsersAPI = "http://3.108.185.65:9002/api/getUsers";
+  const getUsersWithJobsAPI = "http://3.108.185.65:9002/api/getUsersWithJobs";
+  const updateJobsDateAPI = "http://3.108.185.65:9002/api/updateJobsDate";
+  const getLastJobsDateAPI = "http://3.108.185.65:9002/api/getLastJobsDate";
   const importerListToAssignJobs =
-    "http://65.2.35.62:9002/api/importerListToAssignJobs";
-  const getYearsAPI = "http://65.2.35.62:9002/api/getYears";
-  const removeUserAPI = "http://65.2.35.62:9002/api/remove-user";
-  const reportFieldsAPI = "http://65.2.35.62:9002/api/getReportFields";
-  const sendOtpAPI = "http://65.2.35.62:9002/api/sendOtp";
+    "http://3.108.185.65:9002/api/importerListToAssignJobs";
+  const getYearsAPI = "http://3.108.185.65:9002/api/getYears";
+  const removeUserAPI = "http://3.108.185.65:9002/api/remove-user";
+  const reportFieldsAPI = "http://3.108.185.65:9002/api/getReportFields";
+  const sendOtpAPI = "http://3.108.185.65:9002/api/sendOtp";
   const sendChangePasswordOtpAPI =
-    "http://65.2.35.62:9002/api/sendChangePasswordOtp";
-  const getJobAPI = "http://65.2.35.62:9002/api/getJob";
-  const feedbackAPI = "http://65.2.35.62:9002/api/feedback";
+    "http://3.108.185.65:9002/api/sendChangePasswordOtp";
+  const getJobAPI = "http://3.108.185.65:9002/api/getJob";
+  const feedbackAPI = "http://3.108.185.65:9002/api/feedback";
   const getAssignedImporterAPI =
-    "http://65.2.35.62:9002/api/getAssignedImporter";
-  const removeJobsAPI = "http://65.2.35.62:9002/api/removeJobs";
-  const changePasswordAPI = "http://65.2.35.62:9002/api/changePassword";
-  const downloadReportAPI = "http://65.2.35.62:9002/api/downloadReport";
+    "http://3.108.185.65:9002/api/getAssignedImporter";
+  const removeJobsAPI = "http://3.108.185.65:9002/api/removeJobs";
+  const changePasswordAPI = "http://3.108.185.65:9002/api/changePassword";
+  const downloadReportAPI = "http://3.108.185.65:9002/api/downloadReport";
 
   return {
     loginAPI,
